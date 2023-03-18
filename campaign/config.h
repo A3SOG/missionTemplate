2a633b41a2566657b925ad448b82f6660c1d05c4
Jacob schmidt <innovativestudios@outlook.com>
2023-03-17 19:58:36 -0500

    Update 03172023@2000

--- campaign/config.h
+++ campaign/config.h
@@ -1,17 +1,6 @@
 class Params {
-	#define OP_BUDGET_MIN 0
-	#define OP_BUDGET_MAX 120000000
-	#define OP_BUDGET_DEFAULT 12000000
 	#include "params\paramOpBudget.h"
-
-	#define DT_MIN 10
-	#define DT_MAX 300
-	#define DT_DEFAULT 10
 	#include "params\paramDT.h"
-
-	#define MULTIPLYR_MIN 1
-	#define MULTIPLYR_MAX 3
-	#define MULTIPLYR_DEFAULT 1
 	#include "params\paramMultiplyr.h"
 };
 
