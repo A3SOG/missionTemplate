cb2668d5f927c9f38add1b5544022387cd6ee429
Jacob Schmidt <innovativestudios@outlook.com>
2024-03-14 01:39:13 -0500

    Hotfix 03142024@0140

--- operation/params/paramPDBMode.h
+++ operation/params/paramPDBMode.h
@@ -1,6 +1,6 @@
 class PDB_MODE {
 	title = "Persistent Database Mode:";
 	values[] = {0, 1, 2};
-	texts[] = {"ProfileNamespace", "ArmaRedis", "Pythia"};
+	texts[] = {"ProfileNamespace", "ArmaRedis", "LiteDB"};
 	default = 0;
 };
